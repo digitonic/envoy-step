036b5b8b761e9fa444fb4ffa1f5980e4dfd455ca
falko189 <daniele@digitonic.co.uk>
2020-12-10 12:58:52 +0000

    Removed debug info

--- main.sh
+++ main.sh
@@ -8,10 +8,6 @@ echo ${{PRIVATE_KEY}} | base64 -d > $PRIVATEKEY_PATH
 
 eval `ssh-agent -s`
 
-cat $PRIVATEKEY_PATH
-
 ssh-add $PRIVATEKEY_PATH
 
 ssh-keyscan -H ${{INSTANCE_IP}} >> ~/.ssh/known_hosts
-
-cat ~/.ssh/known_hosts
